34ce23b19ca8d28e86a89ceebeaa6aaf120e8236
gking2224 <gking2224@gmail.com>
2014-12-21 21:50:52 +0000

    Minor changes

--- processData.R
+++ processData.R
@@ -1,8 +1,16 @@
 dtc <- raw.data[,-c(1,3,4,5,6,9,10,11,12,13,14,15,16,17,18,19,20,21,22,29,30,31,32,33,34,35,36,37)]
-dtc[,"BGN_DATE"] <- as.Date(dtc[,"BGN_DATE"], "%m/%d/%Y %H:%M:%S")
-dtc$year <- as.POSIXlt(as.Date(dtc[,"BGN_DATE"], "%m/%d/%Y %H:%M:%S"))$year+1900
-dtc <- dtc[dtc$PROPDMGEXP %in% c("B", "M", "K", "", NA) &
-           dtc$CROPDMGEXP %in% c("B", "M", "K", "", NA),]
+
+date_format <- "%m/%d/%Y %H:%M:%S"
+dtc[,"BGN_DATE"] <- as.Date(dtc[,"BGN_DATE"], date_format)
+dtc$year <- as.POSIXlt(dtc[,"BGN_DATE"])$year+1900
+
+
+
+exp <- c("k", "K", "m", "M", "b", "B", "", NA)
+table(dtc$CROPDMGEXP %in% exp)
+table(dtc$PROPDMGEXP %in% exp)
+dtc <- dtc[dtc$PROPDMGEXP %in% exp &
+           dtc$CROPDMGEXP %in% exp,]
 dtc$PROPDMGEXP <- factor(dtc$PROPDMGEXP)
 dtc$CROPDMGEXP <- factor(dtc$CROPDMGEXP)
 dtc$PROPDMGNUM <- mapply(makenum, dtc$PROPDMG, dtc$PROPDMGEXP)
