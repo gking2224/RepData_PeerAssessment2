bfa18bd41062b65c131905674c99108381893e39
gking2224 <gking2224@gmail.com>
2014-12-21 18:58:55 +0000

    Use ggplot for first figure, size plots

--- downloadData.R
+++ downloadData.R
@@ -4,5 +4,5 @@ url <- "https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2FStormData.csv.bz2
 if (!file.exists(filename)) {
     download.file(url=url, destfile = filename, method="curl")
 }
-raw.data.full <- read.csv(
+raw.data <- read.csv(
     filename, header=TRUE, na.strings=c(""))
--- processData.R
+++ processData.R
@@ -1,4 +1,3 @@
-raw.data <- raw.data.full# [rbinom(nrow(raw.data.full),1,0.2)==1,]
 dtc <- raw.data[,-c(1,3,4,5,6,9,10,11,12,13,14,15,16,17,18,19,20,21,22,29,30,31,32,33,34,35,36,37)]
 dtc[,"BGN_DATE"] <- as.Date(dtc[,"BGN_DATE"], "%m/%d/%Y %H:%M:%S")
 dtc$year <- as.POSIXlt(as.Date(dtc[,"BGN_DATE"], "%m/%d/%Y %H:%M:%S"))$year+1900
@@ -9,5 +8,3 @@ dtc$CROPDMGEXP <- factor(dtc$CROPDMGEXP)
 dtc$PROPDMGNUM <- mapply(makenum, dtc$PROPDMG, dtc$PROPDMGEXP)
 dtc$CROPDMGNUM <- mapply(makenum, dtc$CROPDMG, dtc$CROPDMGEXP)
 dtc$TOTALDMG <- dtc$CROPDMGNUM + dtc$PROPDMGNUM
-
-dtc <- dtc[dtc$year >=1995,]
